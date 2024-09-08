edfd7dad272ca7b90b02af32ddd9a334b3ce9e72
ajamest023 <ajamest023@gmail.com>
2024-09-07 23:44:11 -0400

    lkjlkj

--- Homework 1/Debug.java
+++ Homework 1/Debug.java
@@ -1,32 +1,25 @@
 //always start with importing our scanner so we can use it!
 import java.util.Scanner;
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
         //I've already set up a scanner with the name 'sc' for you!
-        Scanner sc = new Scanner(System.in);
-
-        //here's a good working example for you to reference!
-        //first: we ask our user for our prompt
-        System.out.println("What is your name? ");
-        //second: we create a string variable and store the user input back to it
-        String name = sc.nextLine();
-        //third: we print it back out!
-        System.out.println("Hello, " + name + "! ");
-
-
-
-
-
+        
+    
         //finish this one for me by receiving and printing the user's age back to them
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("How old are you? ");
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old.");
 
 
         //can you do some math for me? take these variable and print out the sum of them!
         int num1 = 10;
         int num2 = 37;
         //?
-
+        System.out.print("The sum of these numbers are ");
+        System.out.println( num1 + num2);
         
 
     }
---
+++ Homework 1/test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("What is your first name?");
+       String input = sc.nextLine();
+       System.out.println("Hell0 " + input + ". I just need one more thing from you" );
+
+       System.out.println("What is your last name?");
+       String last = sc.nextLine();
+       System.out.println("Hello " + input + " " + last + ", it is nice to meet you");
+
+        
+
+
+    }}
+
